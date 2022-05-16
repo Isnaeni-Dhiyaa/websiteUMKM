f02c7f3474bc0db04a92431bab4f6ba2d368a3f5
Dhiyaa <dhiyaafathiina@gmail.com>
2022-05-16 22:32:03 +0700

    tambah header

--- WebsiteUMKM/resources/views/template/template.blade.php
+++ WebsiteUMKM/resources/views/template/template.blade.php
@@ -4,9 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link href="{{asset('/css/app.css')}}" rel="stylesheet">
+
+    <title>@yield('judul')</title>
 </head>
 <body>
-    
+    <header>
+        <div class="container">
+            <div class="kepala bg-ijo-gradasi">
+                <div class="batas">
+                    <div class="" id="logo">
+                        <a></a>
+                    </div>
+                    <div class="" id="nama-toko">
+                        <h2>OMAH KOPI TARIK TUNTANG</h2>
+                    </div>
+                    <div class="" id="link">
+                        
+                    </div>
+                    <div class="" id="masuk">
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+    </header>
+    <footer>
+
+    </footer>
 </body>
 </html>
